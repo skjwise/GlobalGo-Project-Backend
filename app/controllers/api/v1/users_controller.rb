dd1de9981eacafb394e23ec2771c5b9a89f3ae42
skjwise <skjwise@hotmail.com>
2020-01-17 11:08:53 +0000

    updated user serializer and controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,50 +6,10 @@ class Api::V1::UsersController < ApplicationController
         }
     end
 
-    def profile
-        render json: { user: UserSerializer.new(current_user) }, status: :accepted
-    end
-    
-    # def create
-    # user = User.new(user_params)
-    # cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    # user.password_digest = BCrypt::Password.create(params[:password], cost: cost)
-    #     if user.save
-    #         token = encode_token(user_id: user.id)
-    #         render json: { user:   UserSerializer.new(user), jwt: token }, status: :created
-    #     else
-    #         render json: {error: 'failed to create user. please try another username' }, status: :not_acceptable
-    #     end
+    # def profile
+    #     render json: { user: UserSerializer.new(current_user) }, status: :accepted
     # end
     
-    # def check_star
-    # starred = false
-    # project_id = params[:project_id].to_i
-    # user = User.find(params[:user_id])
-    # user_projects = @user.projects
-
-    # if user_projects.length > 0
-    #     user_projects.each do |project|
-    #         if project[:id] == project_id
-    #         starred = true
-    #         break
-    #         end
-    #     end
-    #     else
-    #     UserStarredProject.all.each do |star|
-    #     if project_id == star.project_id && user.id == star.user_id
-    #         starred = true
-    #         break
-    #     end
-    #     end
-    # end
-
-    # if starred
-    #     render json: {status: 'Project is starred.'}
-    # else
-    #     render json: {status: 'Project is not starred.'}
-    # end
-    # end
     
     def update
     user = User.find(user_params[:id])
@@ -71,7 +31,6 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def login
-        # byebug
         user = User.find_by(username: user_login_params[:username])
         if user && user.authenticate(user_login_params[:password])
             render json: { user: UserSerializer.new(user), token: issue_token(user_id: user.id) }, status: :accepted
@@ -95,7 +54,7 @@ class Api::V1::UsersController < ApplicationController
     #     :email, :default_country, :theme1, :theme2, :theme3)
     # end
     def user_params
-        params.permit(:id, :username, :password, :first_name, :last_name, :email)
+        params.permit(:id, :username, :password, :email)
     end
 
     def user_login_params
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :password_digest, :email, :default_country, 
-  :first_name, :last_name, :theme1, :theme2, :theme3, :projects
+  attributes :id, :username, :password_digest, :email 
+  
+  # :default_country, :first_name, :last_name, :theme1, :theme2, :theme3, :projects
 
   def self.all
     User.all.map{|d| UserSerializer.new(d)}
